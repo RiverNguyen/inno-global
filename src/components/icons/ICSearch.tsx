031fd8b62585ec2ba82be511f777c09eb4a7ec76
duc82 <duccdht123@gmail.com>
2026-02-04 16:25:06 +0700

    [update]: add project list ui

---
+++ src/components/icons/ICSearch.tsx
@@ -0,0 +1,29 @@
+export default function ICSearch(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      {...props}
+    >
+      <path
+        d='M7.66634 13.9997C11.1641 13.9997 13.9997 11.1641 13.9997 7.66634C13.9997 4.16854 11.1641 1.33301 7.66634 1.33301C4.16854 1.33301 1.33301 4.16854 1.33301 7.66634C1.33301 11.1641 4.16854 13.9997 7.66634 13.9997Z'
+        stroke='#090909'
+        strokeOpacity='0.6'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M14.6663 14.6663L13.333 13.333'
+        stroke='#090909'
+        strokeOpacity='0.6'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
---
+++ src/components/shared/ProjectCard/index.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import Image from 'next/image'
+import { useTranslations } from 'next-intl'
+
+import ICChevronDown from '@/components/icons/ICChevronDown'
+import { Link } from '@/i18n/navigation'
+import { cn } from '@/lib/utils'
+
+interface IProject {
+  image: string
+  title: string
+  investor: string
+  location: string
+  link: string
+  imageHover: string
+}
+
+interface IProjectCard {
+  project: IProject
+  wrapperClassname?: string
+}
+
+export default function ProjectCard({ project, wrapperClassname }: IProjectCard) {
+  const t = useTranslations('ProjectListPage')
+
+  return (
+    <Link
+      href={project.link}
+      className={cn('relative overflow-hidden group', wrapperClassname)}
+    >
+      <div className='relative flex items-center justify-center w-full h-[15.15625rem] rounded-[0.20833rem] xsm:h-[11.30984rem] xsm:rounded-[0.1576rem] overflow-hidden'>
+        <Image
+          src={project.image}
+          alt={project.title}
+          width={460}
+          height={291}
+          className='absolute top-0 left-0 size-full object-cover z-1 transition-all duration-500 ease-[cubic-bezier(0.44,0,0,0.99)] lg:group-hover:opacity-0 lg:group-hover:scale-120'
+        />
+        <Image
+          src={project.imageHover}
+          alt={project.title}
+          width={460}
+          height={291}
+          className='absolute top-0 left-0 size-full object-cover transition-all duration-500 ease-[cubic-bezier(0.44,0,0,0.99)] lg:group-hover:scale-120'
+        />
+      </div>
+      <div className='pt-[0.72917rem]'>
+        <div className='flex items-center justify-between mb-[0.3125rem]'>
+          <h3 className='font-open-sans text-[0.9375rem] font-semibold leading-[150%] text-[#090909]'>
+            {project?.title}
+          </h3>
+          <div className='lg:opacity-0 inline-flex items-center space-x-[0.20833rem] font-open-sans text-[0.72917rem] font-normal leading-[150%] text-[#D32F2F] transition-all duration-500 ease-[cubic-bezier(0.44,0,0,0.99)] lg:group-hover:opacity-100 xsm:hidden'>
+            <span className='[text-box-edge:cap_alphabetic] [text-box-trim:trim-both]'>
+              Xem chi tiết
+            </span>
+            <ICChevronDown className='size-[0.72917rem] shrink-0 -rotate-90' />
+          </div>
+        </div>
+        <div className='flex flex-col space-y-[0.46875rem]'>
+          <div className='flex items-center space-x-[0.3125rem] font-open-sans text-[0.72917rem] leading-[150%] text-[rgba(9,9,9,0.6)]'>
+            <Image
+              src='/projects/d-brifecase-tick.png'
+              alt=''
+              width={16}
+              height={16}
+              className='size-[0.83333rem] shrink-0 object-cover'
+            />
+            <span className='[text-box-edge:cap_alphabetic] [text-box-trim:trim-both]'>
+              {t('investor')}:
+            </span>
+            <span className='[text-box-edge:cap_alphabetic] [text-box-trim:trim-both]'>
+              {project?.investor}
+            </span>
+          </div>
+          <div className='flex items-center space-x-[0.3125rem] font-open-sans text-[0.72917rem] leading-[150%] text-[rgba(9,9,9,0.6)]'>
+            <Image
+              src='/projects/d-location.png'
+              alt=''
+              width={16}
+              height={16}
+              className='size-[0.83333rem] shrink-0 object-cover'
+            />
+            <span className='[text-box-edge:cap_alphabetic] [text-box-trim:trim-both]'>
+              {t('location')}:
+            </span>
+            <span className='[text-box-edge:cap_alphabetic] [text-box-trim:trim-both]'>
+              {project?.location}
+            </span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
