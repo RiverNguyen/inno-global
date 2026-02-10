ceab158311a24db1786ee0867574fcc72fcde2c5
sylph03 <sylph203@gmail.com>
2026-02-10 16:19:35 +0700

    fix z-index header menu in page listproject

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -74,11 +74,11 @@ export default function Header({ data }: { data: { logo: IAcfImage; menus: IMenu
       <div
         onClick={handleCloseSearch}
         className={cn(
-          'xsm:hidden pointer-events-none fixed top-0 left-0 z-[98] size-full bg-black/60 opacity-0 transition-opacity duration-200',
+          'xsm:hidden pointer-events-none fixed top-0 left-0 z-[101] size-full bg-black/60 opacity-0 transition-opacity duration-200',
           openSearch && 'pointer-events-auto opacity-100',
         )}
       ></div>
-      <header className='xsm:h-[2.92rem] xsm:shadow-[0_4px_30px_0_rgba(0,_0,_0,_0.06)] flex-y-center xsm:bg-white/80 fixed top-0 left-0 z-[99] h-[3.65rem] w-full bg-[#DADADA] shadow-[0_0_30px_0_rgba(0,_0,_0,_0.06)] backdrop-blur-[4px]'>
+      <header className='xsm:h-[2.92rem] xsm:shadow-[0_4px_30px_0_rgba(0,_0,_0,_0.06)] flex-y-center xsm:bg-white/80 fixed top-0 left-0 z-[102] h-[3.65rem] w-full bg-[#DADADA] shadow-[0_0_30px_0_rgba(0,_0,_0,_0.06)] backdrop-blur-[4px]'>
         <div
           className={cn(
             'xsm:px-[0.83333rem] container flex items-center justify-between',
@@ -257,7 +257,7 @@ export default function Header({ data }: { data: { logo: IAcfImage; menus: IMenu
       {/* mobile menu */}
       <div
         className={cn(
-          'fixed top-0 left-0 z-[98] h-screen w-full overflow-y-auto bg-white px-[0.83333rem] pt-[calc(2.92rem+0.31rem)] transition-[transform,opacity] duration-200 ease-out will-change-transform sm:hidden pb-[20vh]',
+          'fixed top-0 left-0 z-[101] h-screen w-full overflow-y-auto bg-white px-[0.83333rem] pt-[calc(2.92rem+0.31rem)] transition-[transform,opacity] duration-200 ease-out will-change-transform sm:hidden pb-[20vh]',
           openMenu ? 'translate-x-0 opacity-100 pointer-events-auto' : 'translate-x-full opacity-0 pointer-events-none',
         )}
       >
@@ -323,7 +323,7 @@ export default function Header({ data }: { data: { logo: IAcfImage; menus: IMenu
       {/* search */}
       <div
         className={cn(
-          'fixed top-0 left-0 z-[98] h-screen w-full overflow-y-auto bg-white px-[0.83333rem] pt-[calc(2.92rem+0.31rem)] transition-[transform,opacity] duration-200 ease-out will-change-transform sm:hidden',
+          'fixed top-0 left-0 z-[101] h-screen w-full overflow-y-auto bg-white px-[0.83333rem] pt-[calc(2.92rem+0.31rem)] transition-[transform,opacity] duration-200 ease-out will-change-transform sm:hidden',
           openSearch
             ? 'translate-x-0 opacity-100 pointer-events-auto'
             : 'translate-x-full opacity-0 pointer-events-none',
