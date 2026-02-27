cc857d550210609bd53adb3138d500a12f53d6f4
River Nguyen <ngiang1309@gmail.com>
2026-02-27 09:29:17 +0700

    Refactor layout and styling in user components: update className formatting for consistency, adjust CSS for better responsiveness, and ensure proper spacing in global styles.

--- src/app/[locale]/(vi)/(user)/layout.tsx
+++ src/app/[locale]/(vi)/(user)/layout.tsx
@@ -10,7 +10,7 @@ const UserLayout = ({ children }: { children: React.ReactNode }) => {
         }
         `}
       </style>
-      <main className="bg-[#F8F8F8] h-[calc(100vh-4.95rem)]">
+      <main className='bg-[#F8F8F8] h-[calc(100vh-4.95rem)]'>
         <section className='max-w-[66.25rem] mx-auto flex space-x-[1.25rem] pt-[6.15rem]'>
           <Aside />
           {children}
--- src/app/[locale]/(vi)/(user)/thong-tin-tai-khoan/page.tsx
+++ src/app/[locale]/(vi)/(user)/thong-tin-tai-khoan/page.tsx
@@ -25,22 +25,27 @@ const DashboardPage = () => {
   ]
 
   return (
-    <div className="p-[1.46rem] pb-[3.3875rem] bg-white flex-1">
-      <h1 className="text-[#090909] text-[1.46rem] font-semibold leading-[1.5] mb-[2.08rem]">Thông tin tài khoản</h1>
-      <div className="flex space-x-[2.08rem]">
-        <div className="size-[12.44792rem] rounded-full relative overflow-hidden">
+    <div className='p-[1.46rem] pb-[3.3875rem] bg-white flex-1'>
+      <h1 className='text-[#090909] text-[1.46rem] font-semibold leading-[1.5] mb-[2.08rem]'>Thông tin tài khoản</h1>
+      <div className='flex space-x-[2.08rem]'>
+        <div className='size-[12.44792rem] rounded-full relative overflow-hidden'>
           <Image
             src={session?.user?.avatar_512 || ''}
             alt={session?.user?.first_name || ''}
-            className="size-full object-cover"
+            className='size-full object-cover'
             fill
           />
         </div>
-        <div className="grid grid-cols-2 items-start gap-x-[1.56rem]">
+        <div className='grid grid-cols-2 items-start gap-x-[1.56rem]'>
           {infoItems.map((item) => (
-            <div key={item.label} className='w-[16.06rem]'>
-              <p className="text-[#090909]/60 text-[0.73rem] leading-[1.5]">{item.label}</p>
-              <p className="text-[0.83rem] font-semibold leading-[1.3] tracking-[-0.00833rem] mt-[0.32rem]">{item.value}</p>
+            <div
+              key={item.label}
+              className='w-[16.06rem]'
+            >
+              <p className='text-[#090909]/60 text-[0.73rem] leading-[1.5]'>{item.label}</p>
+              <p className='text-[0.83rem] font-semibold leading-[1.3] tracking-[-0.00833rem] mt-[0.32rem]'>
+                {item.value}
+              </p>
               <hr className='mt-[0.62rem] border-[#1A1A1A]/15' />
             </div>
           ))}
