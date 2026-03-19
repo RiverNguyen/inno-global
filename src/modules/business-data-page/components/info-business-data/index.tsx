4eaf2f5f11db7275263bd754cb762b431e0c03e9
River Nguyen <ngiang1309@gmail.com>
2026-03-19 12:01:06 +0700

    Adjust CSS for table cell alignment in InfoBusinessData component to enhance visual consistency and improve user experience.

--- src/modules/business-data-page/components/info-business-data/index.tsx
+++ src/modules/business-data-page/components/info-business-data/index.tsx
@@ -8,11 +8,11 @@ const InfoBusinessData = ({ acfOverView }: { acfOverView: IBDProjectOverview[] }
           {acfOverView?.map((data, index) => {
             return (
               <tr key={index}>
-                <td className='xsm:p-[0.41667rem] xsm:w-[6.17rem] xsm:mb-body-14-r xsm:font-semibold text-[#090909]/80 pc-body-16-s uppercase py-[1.46rem] w-[18.02rem] px-[1.67rem] align-top border-1 border-[#09090914]/80'>
+                <td className='xsm:p-[0.41667rem] xsm:w-[6.17rem] xsm:mb-body-14-r xsm:font-semibold text-[#090909]/80 pc-body-16-s uppercase py-[1.46rem] w-[18.02rem] px-[1.67rem] xsm:align-top border-1 border-[#09090914]/80'>
                   {data?.label}
                 </td>
                 <td
-                  className='xsm:p-[0.625rem] xsm:mb-body-14-r text-[#090909]/80 pc-body-20-r py-[1.46rem] px-[1.67rem] border-1 border-[#09090914]/80 [&_a]:text-[#000DFF] [&_a]:underline'
+                  className='xsm:p-[0.625rem] xsm:mb-body-14-r text-[#090909]/80 pc-body-20-r py-[1.46rem] px-[1.67rem] border-1 border-[#09090914]/80 [&_a]:text-[#000DFF] [&_a]:underline xsm:align-top'
                   dangerouslySetInnerHTML={{ __html: data?.value || '' }}
                 ></td>
               </tr>
